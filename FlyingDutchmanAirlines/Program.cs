ca82c1bac80c6d32caa6f55997d9c9df3557f213
haozhang <haozhan@microsoft.com>
2023-02-28 20:14:06 +0800

    initialize project as web application

---
+++ FlyingDutchmanAirlines/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace FlyingDutchmanAirlines
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            InitializeHost();
+        }
+
+        private static void InitializeHost()
+        {
+            Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(builder =>
+                {
+                    // use startup
+                    builder.UseStartup<Startup>();
+                    builder.UseUrls("http://0.0.0.0:8080");
+                })
+                .Build()
+                .Run();
+        }
+    }
+}
---
+++ FlyingDutchmanAirlines/Startup.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlyingDutchmanAirlines;
+
+public class Startup
+{
+    public void Configure(IApplicationBuilder app)
+    {
+        // let request routes to endpoint
+        app.UseRouting();
+        // endpoint maps to controller
+        app.UseEndpoints(endpoint => endpoint.MapControllers());
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // add all controllers
+        services.AddControllers();
+    }
+}
